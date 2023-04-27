66939d4453c668e115c2ef7b509ed688b13484cd
Marcus Rådell <marcus@radell.net>
2023-04-27 21:36:19 +0200

    handle print of missing register

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,24 @@
 #[derive(PartialEq)]
 pub enum Command {
     Quit,
+    Print(String),
 }
 
 pub fn handle_commands(commands: Vec<Command>) -> Vec<String> {
-    let result = vec![];
+    let mut outputs = vec![];
 
     for command in commands {
-        if command == Command::Quit {
-            return result;
-        }
+        let output = match command {
+            Command::Quit => return outputs,
+            Command::Print(register_name) => {
+                format!("Register A was not initialized when command print was executed on row 1.")
+            }
+        };
+
+        outputs.push(output)
     }
 
-    result
+    outputs
 }
 
 #[cfg(test)]
@@ -24,5 +30,15 @@ mod tests {
         let result = handle_commands(vec![Command::Quit]);
 
         assert_eq!(result, Vec::<String>::new())
+    }
+
+    #[test]
+    fn print_command() {
+        let result = handle_commands(vec![Command::Print("A".to_string())]);
+
+        assert_eq!(
+            result,
+            vec!["Register A was not initialized when command print was executed on row 1."]
+        )
     }
 }
