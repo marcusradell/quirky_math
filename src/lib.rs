4f50aadc7b59254c817edd1c77f157468c698d4d
Marcus Rådell <marcus@radell.net>
2023-04-27 21:20:03 +0200

    refactor command code into lib

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+#[derive(PartialEq)]
+pub enum Command {
+    Quit,
+}
+
+pub fn handle_commands(commands: Vec<Command>) -> Vec<String> {
+    let result = vec![];
+
+    for command in commands {
+        if command == Command::Quit {
+            return result;
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::handle_commands;
+
+    #[test]
+    fn quit_command() {
+        let result = handle_commands(vec![]);
+
+        assert_eq!(result, Vec::<String>::new())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,9 @@
-#[derive(PartialEq)]
-enum Command {
-    Quit,
-}
+use quirky_math::{handle_commands, Command};
 
 fn main() {
     let commands = vec![Command::Quit];
 
-    let result = handle_commands(commands);
+    let output = handle_commands(commands);
 
-    println!("{result:?}");
-}
-
-fn handle_commands(commands: Vec<Command>) -> Vec<String> {
-    let result = vec![];
-
-    for command in commands {
-        if command == Command::Quit {
-            return result;
-        }
-    }
-
-    result
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::handle_commands;
-
-    #[test]
-    fn quit_command() {
-        let result = handle_commands(vec![]);
-
-        assert_eq!(result, Vec::<String>::new())
-    }
+    println!("{output:?}");
 }
