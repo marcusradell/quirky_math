ce115fff6e23aa950e05d114ca3676623f86910f
Marcus Rådell <marcus@radell.net>
2023-04-27 21:14:08 +0200

    handle quit command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+#[derive(PartialEq)]
+enum Command {
+    Quit,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let commands = vec![Command::Quit];
+
+    let result = handle_commands(commands);
+
+    println!("{result:?}");
+}
+
+fn handle_commands(commands: Vec<Command>) -> Vec<String> {
+    let result = vec![];
+
+    for command in commands {
+        if command == Command::Quit {
+            return result;
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::handle_commands;
+
+    #[test]
+    fn quit_command() {
+        let result = handle_commands(vec![]);
+
+        assert_eq!(result, Vec::<String>::new())
+    }
 }
