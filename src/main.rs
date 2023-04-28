a321bca0789240ea6fdaf581c7e3fe8d3105d3e3
Marcus Rådell <marcus@radell.net>
2023-04-28 22:00:42 +0200

    add commands for output 5,3,6

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use quirky_math::{handle_commands, Command};
 
 fn main() {
-    let commands = vec![
+    let commands_536 = vec![
+        Command::Add("A".to_string(), 2),
+        Command::Add("A".to_string(), 3),
+        Command::Print("A".to_string()),
+        Command::Add("B".to_string(), 5),
+        Command::Subtract("B".to_string(), 2),
+        Command::Print("B".to_string()),
         Command::Add("A".to_string(), 1),
         Command::Print("A".to_string()),
-        Command::Print("B".to_string()),
         Command::Quit,
     ];
 
-    let outputs = handle_commands(commands);
+    let outputs = handle_commands(commands_536);
 
     for output in outputs {
         println!("{output}");
